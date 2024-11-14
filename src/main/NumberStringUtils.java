e3a1be58ce70dcf9b1eb7bf6d5ddb1f8fb5a7678
Ubrizze <132262101+Ubrizze@users.noreply.github.com>
2024-11-14 22:43:58 +0100

    Create NumberStringUtils.java

---
+++ src/main/NumberStringUtils.java
@@ -0,0 +1,34 @@
+public class NumberStringUtils {
+    
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static boolean isPalindrome(String str) {
+        if (str == null) {
+            return false;
+        }
+                
+        String cleanStr = str.replaceAll("\\s+", "").toLowerCase();
+        int left = 0;
+        int right = cleanStr.length() - 1;
+        
+        while (left < right) {
+            if (cleanStr.charAt(left) != cleanStr.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
